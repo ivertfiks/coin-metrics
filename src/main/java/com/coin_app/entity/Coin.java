28e67fb3e4553f4a92a130ff54a1fdb908cc2b31
Ivan Kovalov <ivertfiks42@gmail.com>
2024-10-15 21:36:38 +0200

    adjust several checkpoints in class Coin

--- src/main/java/com/coin_app/entity/Coin.java
+++ src/main/java/com/coin_app/entity/Coin.java
@@ -24,6 +24,24 @@ public class Coin {
     @JsonProperty("market_cap_rank")
     private int marketCapRank;
 
+    @JsonProperty("market_cap")
+    private long marketCapUsd;
+
+    @JsonProperty("fully_diluted_valuation")
+    private long fullyDilutedValuationUsd;
+
+    @JsonProperty("total_volume")
+    private long totalVolumeUsd;
+
+    @JsonProperty("circulating_supply")
+    private long circulatingSupply;
+
+    @JsonProperty("total_supply")
+    private long totalSupply;
+
+    @JsonProperty("max_supply")
+    private long maxSupply;
+
     @JsonProperty("high_24h")
     private double highDay;
 
@@ -42,6 +60,40 @@ public class Coin {
         if (currentPriceNode != null && currentPriceNode.has("usd")) {
             this.currentPrice = currentPriceNode.get("usd").asDouble();
         }
+
+        JsonNode marketCapNode = marketData.get("market_cap");
+        if (marketCapNode != null) {
+            if (marketCapNode.has("usd")) {
+                this.marketCapUsd = marketCapNode.get("usd").asLong();
+            }
+        }
+
+        JsonNode fullyDilutedValuationNode = marketData.get("fully_diluted_valuation");
+        if (fullyDilutedValuationNode != null && fullyDilutedValuationNode.has("usd")) {
+            this.fullyDilutedValuationUsd = fullyDilutedValuationNode.get("usd").asLong();
+        }
+
+        JsonNode totalVolumeNode = marketData.get("total_volume");
+        if (totalVolumeNode != null && totalVolumeNode.has("usd")) {
+            this.totalVolumeUsd = totalVolumeNode.get("usd").asLong();
+        }
+
+
+        JsonNode circulatingSupplyNode = marketData.get("circulating_supply");
+        if (circulatingSupplyNode != null) {
+            this.circulatingSupply = circulatingSupplyNode.asLong();
+        }
+
+        JsonNode totalSupplyNode = marketData.get("total_supply");
+        if (totalSupplyNode != null) {
+            this.totalSupply = totalSupplyNode.asLong();
+        }
+
+        JsonNode maxSupplyNode = marketData.get("max_supply");
+        if (maxSupplyNode != null) {
+            this.maxSupply = maxSupplyNode.asLong();
+        }
+
     }
 
     public void setImage(Object image) {
